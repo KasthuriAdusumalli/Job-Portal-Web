31e52d30233ec6fa425dc9bab6cf90c725ef96ed
KasthuriAdusumalli <kasthuriadusumalli401@gmail.com>
2021-09-17 21:07:12 +0530

    Apply job functionality added

--- src/app/candidate/components/search-jobs-list/search-jobs-list.component.ts
+++ src/app/candidate/components/search-jobs-list/search-jobs-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { RestApiService } from '../../../_shared/rest-api.service';
-
+import * as _ from 'lodash';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-search-jobs-list',
   templateUrl: './search-jobs-list.component.html',
@@ -66,5 +67,39 @@ export class SearchJobsListComponent implements OnInit {
 
   clear(table: any) {
     table.clear();
+  }
+  applyjob(jobdata: any) {
+    let job: any = _.pick(jobdata, [
+      'jobCode',
+      'position',
+      'location',
+      'primarySkill',
+      'secondarySkill',
+      'experience',
+    ]);
+    job['otherSkills'] = jobdata.otherSkill || '';
+    job['postedDate'] = new Date().toISOString();
+    this.api
+      .postRequest(job, 'post-apply-job', environment.jobsBaseurl)
+      .subscribe(
+        (res) => {
+          if (res) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Job Applied successfully!!!',
+              text: '',
+              timer: 2000,
+            });
+          }
+        },
+        (err) => {
+          console.log(err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+          });
+        }
+      );
   }
 }
